ef698010aa907a328e928e44284bf76d9e1a8074
tangjia <immrtang@foxmail.com>
2016-12-20 10:52:13 +0800

    delete some

---
+++ src/main/java/org/tj/meta/base/study/TestCollection.java
@@ -0,0 +1,17 @@
+package org.tj.meta.base.study;
+
+import java.util.*;
+
+/**
+ * Created by liwenliwen on 16/12/19.
+ * 此类主要用来学习Collection接口以及派生类
+ */
+public class TestCollection {
+
+    public static void main(String[] args) {
+        List a = new ArrayList();
+        a.add("xixi");
+        a.add("haha");
+
+    }
+}
