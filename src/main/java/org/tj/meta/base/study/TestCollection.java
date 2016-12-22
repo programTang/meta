ac3aa0f4a3c1f8c25288815cbeb415aae912db92
tangjia <tangjia@fahai.com>
2016-12-22 12:55:17 +0800

    引入util包

--- src/main/java/org/tj/meta/base/study/TestCollection.java
+++ src/main/java/org/tj/meta/base/study/TestCollection.java
@@ -1,5 +1,7 @@
 package org.tj.meta.base.study;
 
+import com.sun.jmx.remote.internal.ArrayQueue;
+
 import java.util.*;
 
 /**
@@ -9,12 +11,19 @@ import java.util.*;
 public class TestCollection {
 
     public static void main(String[] args) {
-        List a = new ArrayList();
-        a.add("xixi");
-        a.add("haha");
-        char[] chars = {'a','b','c','d','e'};
-        System.arraycopy(chars,2,chars,3,2);
-        System.out.println(chars.length);
-        Vector
+//        List a = new ArrayList();
+//        a.add("xixi");
+//        a.add("haha");
+//        char[] chars = {'a','b','c','d','e'};
+//        System.arraycopy(chars,2,chars,3,2);
+//        System.out.println(chars.length);
+//        Stack<String> stack = new Stack();
+//        Iterator i = a.iterator();
+//        TestCollection testCollection = new TestCollection();
+//        System.out.println(testCollection.hashCode());
+        Map<String,String> map = new HashMap();
+        map.put("ka","kv");
+        System.out.println(map.get("ka"));
+        StringBuffer
     }
 }
